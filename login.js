968814687e589d75363bb2f7f0433010cb4406cb
alex gnavi <alex.gnavi@gmail.com>
2022-11-02 11:58:12 +0100

    funzione login in file javascript

---
+++ login.js
@@ -0,0 +1,27 @@
+console.log("script ok");
+
+document.getElementById('submit').onclick = function (e) {
+ 
+    var error = false;
+
+    if(check('username')) error = true;
+    if(check('password')) error = true;
+
+    if(error) e.preventDefault();
+
+}
+
+function check(id) {
+
+if(document.getElementById(id).value == '')
+{
+document.getElementById('error_' + id).innerHTML = 'Campo obbligatorio';
+return true;
+}
+else
+{
+document.getElementById('error_' + id).innerHTML = '';
+return false;
+}
+
+}
