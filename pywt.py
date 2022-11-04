be92a5808f6ea06f1447b8f88095406d7d07fa1a
alex gnavi <alex.gnavi@gmail.com>
2022-11-04 09:56:58 +0100

    aggiunto commento token

--- pywt.py
+++ pywt.py
@@ -3,6 +3,7 @@ encoded_jwt = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
 print(encoded_jwt)
 
 jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
+# 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI0M...81gVns6I_j4kSuyuRxlAJBe3pHi-yS2'
 {'some': 'payload'}
 
 payload_data = {
